a82e5a485f37d4231c8a15f7cc047ee91157d550
Katrina Sayaang <50119617+zenkaeser@users.noreply.github.com>
2019-09-16 20:46:44 +0200

    changing the scope of variable weekdayName

--- javascript/javascript1/week2/event-application.js
+++ javascript/javascript1/week2/event-application.js
@@ -1,31 +1,32 @@
 console.log("\n-- Event Application --");
 
 let d = new Date;
-let weekdayName = "";
+
 const todaysDay = d.getDay();
 console.log("Today's day: " + todaysDay);
 
 function getEventWeekday(day) {
   const weekday = (day + todaysDay) % 7;
+  let weekdayName;    //created the variable inside the function so it will not return anything if no parameter is passed.
 
   switch(weekday) {
+    case 0: weekdayName = "Sunday"; break;
     case 1: weekdayName = "Monday"; break;
     case 2: weekdayName = "Tuesday"; break;
     case 3: weekdayName = "Wednesday"; break;
     case 4: weekdayName = "Thursday"; break;
     case 5: weekdayName = "Friday"; break;
     case 6: weekdayName = "Saturday"; break;
-    case 7:
-    case 0: weekdayName = "Sunday"; break;
     default: "Not a day"; 
   }
   return weekdayName;
 }
 
-let days = [" ","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
+let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 
 function getEventWeekday2(day) {
   const weekday2 = (day + todaysDay) % 7;
+  let weekdayName;
 
   switch(weekday2) {
     case 1: weekdayName = days[1]; break;
@@ -34,8 +35,7 @@ function getEventWeekday2(day) {
     case 4: weekdayName = days[4]; break;
     case 5: weekdayName = days[5]; break;
     case 6: weekdayName = days[6]; break;
-    case 7: 
-    case 0: weekdayName = days[7]; break;
+    case 0: weekdayName = days[0]; break;
     default: "Not a day";
   }
   return weekdayName;
