2db07f0238bf7de1d82dbdb5675d944112114e74
Katrina Sayaang <50119617+zenkaeser@users.noreply.github.com>
2019-09-16 20:29:58 +0200

    Fixing the conditional statement of the function

--- javascript/javascript1/week2/student-manager.js
+++ javascript/javascript1/week2/student-manager.js
@@ -1,28 +1,29 @@
 console.log("\n-- Student Manager --");
 console.log("Add your student(s) here: ");
 
-const class07Students = ["kat", "love", "jessa", "james", "kim", "ben"];
+const class07Students = [];
+//["kat", "love", "jessa", "james", "kim", "ben"];
 
 function addStudentToClass(studentName) {
   let classSize = class07Students.length;
 
-  if(classSize >= 7 && studentName != "Queen") {
-    return "Cannot add more student to class 07";
-  } else if (!studentName) {
-    return "Cannot add a student with no name - empty string."
-  } 
-  else {
+  if(classSize < 6 || studentName.toLowerCase() == 'queen') {
+    if (!studentName) {
+      return "Cannot add a student with no name - empty string."
+    }
     for(var i=0; i<classSize; i++) {
-      if(studentName != class07Students[i]) {
+      if(studentName.toLowerCase() != class07Students[i].toLowerCase()) {
         continue;
       }
       else {
         return "Student " + studentName + " is already added to the class."
       }
     }
+    return class07Students.push(studentName);
   }
-
-  return class07Students.push(studentName);
+  else {
+    return "Cannot add more student to class 07";
+  }
 }
 
 function getNumberOfStudents() {
