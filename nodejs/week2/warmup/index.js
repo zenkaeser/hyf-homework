3bfc527023ae2419b9ecde2de5df1ef5fd8b445a
Katrina Sayaang <50119617+zenkaeser@users.noreply.github.com>
2020-02-23 19:11:47 +0100

    Update index.js

--- nodejs/week2/warmup/index.js
+++ nodejs/week2/warmup/index.js
@@ -2,15 +2,15 @@ const express = require('express');
 const app = express();
 
 app.get('/numbers/add', (request, response) => {
-    const first = Number(request.query.first);
-    const second = Number(request.query.second);
-    const sum = first + second;
+    const {first, second} = request.query;
+    const sum = Number(first) + Number(second);
+
     response.send(JSON.stringify(sum));
 })
 app.get('/numbers/multiply/:first/:second', (request, response) => {
-    const first = request.params["first"];
-    const second = request.params["second"];
+    const {first, second} = request.params;
     const quotient = first * second;
+    
     response.send(JSON.stringify(quotient));
 })
 
