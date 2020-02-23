4c20b8efb303b6aea8dd6e9622c49fcfd0e05dda
Katrina Sayaang <50119617+zenkaeser@users.noreply.github.com>
2020-02-23 20:14:56 +0100

    Add files via upload

---
+++ nodejs/week3/homework/src/backend/api/reviews.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const app = express();
+const router = express.Router();
+const pool = require("./../database");
+
+router.get("/", (request, response) => {
+  pool.query("SELECT * FROM review", (error, results) => {
+    if (error) throw error;
+    response.send(results);
+  });
+
+});
+
+router.get("/:id", (request, response) => {
+  const id = request.params.id;
+
+  pool.query("SELECT * FROM review WHERE id = ?", id, (error, results) => {
+    if (error) throw error;
+    response.send(results);
+  });
+});
+
+router.post("/", (request, response) => {
+  const r = request.body;
+  const createdDate = new Date();
+  const post = [r.id, r.title, r.description, r.meal_id, r.stars, createdDate];
+
+  pool.query("INSERT INTO review VALUES (?,?,?,?,?,?)", post, (error, results) => {
+    if (error) {
+      if(error.code== "ER_DUP_ENTRY") {
+        console.log("Duplicate Entry");
+      }
+      throw error;
+    }
+    response.send(results);
+  });
+});
+
+router.put("/:id", (request, response) => {
+  const id = request.params.id;
+  const r = request.body;
+
+  pool.query("UPDATE review SET title = ? WHERE id = ?", [r.title, id], (error, results) => {
+    if (error) throw error;
+    response.send(results);
+  });
+});
+
+router.delete("/:id", (request, response) => {
+  const id = Number(request.params.id);
+
+  pool.query("DELETE FROM  review WHERE id = ?",id, (error, results) => {
+    if (error) throw error;
+    response.send(results);
+  });
+});
+
+module.exports = router;
---
+++ nodejs/week3/homework/src/backend/database.js
@@ -0,0 +1,28 @@
+const mysql = require("mysql");
+require("dotenv").config();
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
+
+connection.connect((err, connection) => {
+  if (err) {
+    if (err.code === "PROTOCOL_CONNECTION_LOST") {
+      console.error("Database connection was closed.");
+    }
+    if (err.code === "ER_CON_COUNT_ERROR") {
+      console.error("Database has too many connections.");
+    }
+    if (err.code === "ECONNREFUSED") {
+      console.error("Database connection was refused.");
+    }
+    console.error(err);
+  }
+  if (connection)
+  return;
+});
+
+module.exports = connection;
