d68556e24b5af61e6dacd7ea352ed00566ecb3ca
Katrina Sayaang <50119617+zenkaeser@users.noreply.github.com>
2020-03-01 17:57:05 +0100

    Updated update/edit method

--- react/week3/my-app/src/components/todoListItem.js
+++ react/week3/my-app/src/components/todoListItem.js
@@ -10,20 +10,20 @@ export const TodoListItem = ({
 }) => {
   const [checked, setChecked] = useState(false);
   const [toEdit, setToEdit] = useState(false);
-  const [descr, setDescription] = useState(description);
+  const [descr, setDescription] = useState("");
 
   const handleChecked = () => {
     setChecked(!checked);
   };
-
   const handleEdit = () => {
     setToEdit(!toEdit);
+    setDescription(description);
   };
-  const handleUpdate = () => {
+  const handleUpdate = id => {
     setDescription(descr);
     NewDescription = descr;
     setToEdit(!toEdit);
-    onEdit(descr, id + 1);
+    onEdit(id);
   };
 
   const str = description + " | " + deadline;
@@ -32,7 +32,6 @@ export const TodoListItem = ({
       <li className="todoListItem">
         {toEdit ? (
           <input
-            placeholder="description"
             value={descr}
             onChange={event => setDescription(event.target.value)}
           />
@@ -44,7 +43,7 @@ export const TodoListItem = ({
         )}
         <button onClick={onDelete}>Delete</button>
         {toEdit ? (
-          <button onClick={() => handleUpdate()}>Update</button>
+          <button onClick={() => handleUpdate(id)}>Update</button>
         ) : (
           <button onClick={() => handleEdit()}>Edit</button>
         )}
